cdd569d3ff7bf6a6b43fc3a3d257af81a166345f
andro <kroxitrock@gmail.com>
2023-12-08 18:41:20 +0100

    Show list of articcles

--- app/src/main/java/es/upm/reader/news/MainActivity.kt
+++ app/src/main/java/es/upm/reader/news/MainActivity.kt
@@ -2,20 +2,33 @@ package es.upm.reader.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ListView
 import androidx.lifecycle.lifecycleScope
+import es.upm.reader.news.model.Article
 import es.upm.reader.news.serice.ArticlesService
 import es.upm.reader.news.util.ApplicationProperties
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var articlesListView: ListView
+    private var articles: List<Article> = emptyList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         ApplicationProperties.loadProperties(baseContext.assets.open("application.properties"))
+        articlesListView = findViewById<ListView>(R.id.articlesList)
+
+
         lifecycleScope.launch {
-            val articles = ArticlesService.getArticles()
-            println(articles)
+            articles = ArticlesService.getArticles() ?: emptyList()
+            val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, articles.map { a -> a.title })
+            articlesListView.adapter = arrayAdapter
         }
     }
 }
