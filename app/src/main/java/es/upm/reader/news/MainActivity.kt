dacc6addb26f3f9c4cc65a83978a17a30e19ee67
andro <kroxitrock@gmail.com>
2023-12-08 16:04:15 +0100

    Cleanup article fetching

--- app/src/main/java/es/upm/reader/news/MainActivity.kt
+++ app/src/main/java/es/upm/reader/news/MainActivity.kt
@@ -2,8 +2,10 @@ package es.upm.reader.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import es.upm.reader.news.serice.ArticlesService
 import es.upm.reader.news.util.ApplicationProperties
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         ApplicationProperties.loadProperties(baseContext.assets.open("application.properties"))
-        ArticlesService.getArticles()
+        lifecycleScope.launch {
+            val articles = ArticlesService.getArticles()
+            println(articles)
+        }
     }
 }
--- app/src/main/java/es/upm/reader/news/serice/ArticlesService.kt
+++ app/src/main/java/es/upm/reader/news/serice/ArticlesService.kt
@@ -1,19 +1,19 @@
 package es.upm.reader.news.serice
 
+import es.upm.reader.news.model.Article
 import es.upm.reader.news.repository.ArticlesRepository
 import es.upm.reader.news.repository.ArticlesRepositoryFactory
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 object ArticlesService {
     private val repository: ArticlesRepository = ArticlesRepositoryFactory().getInstance()
 
-    fun getArticles() {
-        runBlocking {
-            launch {
-                val result = repository.getArticles()
-                println(result.body())
-            }
+    suspend fun getArticles(): List<Article>? {
+        val response = repository.getArticles()
+
+        if (response.isSuccessful) {
+            return response.body()
         }
+
+        throw Exception(response.message())
     }
 }
