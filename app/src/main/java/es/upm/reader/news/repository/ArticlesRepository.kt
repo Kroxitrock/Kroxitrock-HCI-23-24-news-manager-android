d631e4cf60fe9b87166f97f93cea43c3ff28d228
andro <kroxitrock@gmail.com>
2023-12-09 20:01:05 +0100

    Add image upload

--- app/src/main/java/es/upm/reader/news/repository/ArticlesRepository.kt
+++ app/src/main/java/es/upm/reader/news/repository/ArticlesRepository.kt
@@ -2,7 +2,9 @@ package es.upm.reader.news.repository
 
 import es.upm.reader.news.model.Article
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ArticlesRepository {
@@ -11,4 +13,7 @@ interface ArticlesRepository {
 
     @GET("/pui-rest-news/article/{id}")
     suspend fun getArticles(@Path(value = "id") id: Int): Response<Article>
+
+    @POST("/pui-rest-news/article")
+    suspend fun saveArticle(@Body article: Article)
 }
