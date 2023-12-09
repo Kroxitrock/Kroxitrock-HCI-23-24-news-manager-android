77028b8836180dba0aba460cef8ebebf0c73f1fc
andro <kroxitrock@gmail.com>
2023-12-09 14:49:57 +0100

    Auth buton icon logic

--- app/src/main/java/es/upm/reader/news/MainActivity.kt
+++ app/src/main/java/es/upm/reader/news/MainActivity.kt
@@ -3,6 +3,7 @@ package es.upm.reader.news
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.ListView
 import androidx.lifecycle.lifecycleScope
 import es.upm.reader.news.adapter.ArticleAdapter
@@ -15,18 +16,49 @@ import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     private lateinit var articlesListView: ListView
+    private lateinit var authButton: ImageView
     private var articles: List<Article> = emptyList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        authButton = findViewById<ImageView>(R.id.authButton)
         ApplicationProperties.loadProperties(baseContext.assets.open("application.properties"))
         articlesListView = findViewById(R.id.articlesList)
 
         handleAutoLogin()
         fetchArticles()
+        handleAuthButton()
     }
+
+    private fun handleAuthButton() {
+        handleAuthButtonIcon()
+        authButton.setOnClickListener{
+            if (LoginService.isLoggedIn()) {
+                LoginService.logout()
+            } else {
+                val loginActivityIntent = Intent(
+                    this@MainActivity,
+                    ArticleDetails::class.java
+                )
+                startActivity(loginActivityIntent)
+            }
+            handleAuthButtonIcon()
+            fetchArticles()
+        }
+    }
+
+    private fun handleAuthButtonIcon() {
+        if (LoginService.isLoggedIn()) {
+            authButton.setImageResource(R.drawable.logout)
+        } else {
+            authButton.setImageResource(R.drawable.login)
+        }
+
+    }
+
 
     private fun handleAutoLogin() {
         if (!ApplicationProperties.getProperty("automatic_login").toBoolean()) {
